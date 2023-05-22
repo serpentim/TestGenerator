f0310b4f526869831e64d3bf3ee99c51ea75da6c
serpentim <54822621+serpentim@users.noreply.github.com>
2023-05-22 14:06:57 +0900

    first commit

---
+++ Array_1D_Gen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGenerator
+{
+    internal class Array_1D_Gen
+    {
+        /*static void Main(string[] args)
+        {
+            // Set the size of the array
+            int size = 10;
+
+            // Create a random number generator
+            Random random = new Random();
+
+            // Generate a random array
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(100); // Generate a random number between 0 and 99
+            }
+
+            // Display the generated array
+            Console.WriteLine("Generated Array:");
+            PrintArray(array);
+
+            Console.ReadLine();
+        }
+
+        static void PrintArray(int[] array)
+        {
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }*/
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestGenerator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OpenArrayContent_Click(object sender, RoutedEventArgs e)
+        {
+            ArrayWindow arrayWindow = new ArrayWindow();
+            MainGrid.Children.Clear();
+            MainGrid.Children.Add(arrayWindow);
+        }
+        private void OpenGraphContent_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void OpenMatrixContent_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
