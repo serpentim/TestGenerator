6869950a44cfa3d83a152a9046e16e8622f7add1
serpentim <54822621+serpentim@users.noreply.github.com>
2023-05-24 15:36:01 +0900

    Add zip. creator

--- OnedimArrayWindow.xaml.cs
+++ OnedimArrayWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,10 +23,20 @@ namespace TestGenerator
     /// </summary>
     public partial class OnedimArrayWindow : UserControl
     {
+        Random random = new Random();
         public OnedimArrayWindow()
         {
             InitializeComponent();
+
+            standardRadioButton.IsChecked = true;
         }
+
+        private void SortOption_Checked(object sender, RoutedEventArgs e)
+        {
+            // Clear the result text box when a sorting option is selected
+            resultOnedim.Text = string.Empty;
+        }
+
         private void GenerateArray_Click(object sender, RoutedEventArgs e)
         {
             if (int.TryParse(sizeTextBox.Text, out int size) &&
@@ -33,71 +44,29 @@ namespace TestGenerator
                 int.TryParse(maxValueTextBox.Text, out int maxValue))
             {
                 int[] numbers = GenerateRandomArray(size, minValue, maxValue);
-                resultOnedim.Text = string.Join(" ", numbers);
-            }
-            else
-            {
-                MessageBox.Show("Пожалуйста, введите корректные значения для длины и диапазона массива.");
-            }
-        }
-        private void GenerateArraySortAsc_Click(object sender, RoutedEventArgs e)
-        {
-            if (int.TryParse(sizeTextBox.Text, out int size) &&
-                int.TryParse(minValueTextBox.Text, out int minValue) &&
-                int.TryParse(maxValueTextBox.Text, out int maxValue))
-            {
-                int[] numbers = GenerateRandomArray(size, minValue, maxValue);
-                Array.Sort(numbers);
-                resultOnedim.Text = string.Join(" ", numbers);
-            }
-            else
-            {
-                MessageBox.Show("Пожалуйста, введите корректные значения для длины и диапазона массива.");
-            }
-        }
-        private void GenerateArraySortDesc_Click(object sender, RoutedEventArgs e)
-        {
-            if (int.TryParse(sizeTextBox.Text, out int size) &&
-                int.TryParse(minValueTextBox.Text, out int minValue) &&
-                int.TryParse(maxValueTextBox.Text, out int maxValue))
-            {
-                int[] numbers = GenerateRandomArray(size, minValue, maxValue);
-                Array.Sort(numbers, (a, b) => b.CompareTo(a)); // Обратный компаратор для сортировки по убыванию
-                resultOnedim.Text = string.Join(" ", numbers);
-            }
-            else
-            {
-                MessageBox.Show("Пожалуйста, введите корректные значения для длины и диапазона массива.");
-            }
-        }
-        private void GenerateArraySortAscAlmost_Click(object sender, RoutedEventArgs e)
-        {
-            if (int.TryParse(sizeTextBox.Text, out int size) &&
-                int.TryParse(minValueTextBox.Text, out int minValue) &&
-                int.TryParse(maxValueTextBox.Text, out int maxValue))
-            {
-                int[] numbers = GenerateRandomArray(size, minValue, maxValue);
-                Array.Sort(numbers);
 
-                // Поиск первого элемента, отличного от последнего элемента в массиве
-                int lastElement = numbers[numbers.Length - 1];
-                int indexToSwap = -1;
-                //int indexToSwap = Array.FindIndex(numbers, x => x != numbers[lastElement]);
-                for (int i = numbers.Length - 2; i >= 0; i--)
+                // Check which sorting options are selected and perform the sorting accordingly
+                if (standardRadioButton.IsChecked == true)
                 {
-                    if (numbers[i] != lastElement)
-                    {
-                        indexToSwap = i;
-                        break;
-                    }
+                    // Do nothing
                 }
-
-                // Поменять последний элемент с найденным элементом
-                if (indexToSwap != -1)
+                else if (sortAscRadioButton.IsChecked == true)
                 {
-                    int temp = numbers[indexToSwap];
-                    numbers[indexToSwap] = numbers[numbers.Length - 1];
-                    numbers[numbers.Length - 1] = temp;
+                    Array.Sort(numbers);
+                }
+                else if (sortDescRadioButton.IsChecked == true)
+                {
+                    Array.Sort(numbers, (a, b) => b.CompareTo(a));
+                }
+                else if (sortAscAlmostRadioButton.IsChecked == true)
+                {
+                    Array.Sort(numbers);
+                    SwapTwoRandomElements(numbers);
+                }
+                else if (sortDescAlmostRadioButton.IsChecked == true)
+                {
+                    Array.Sort(numbers, (a, b) => b.CompareTo(a));
+                    SwapTwoRandomElements(numbers);
                 }
 
                 resultOnedim.Text = string.Join(" ", numbers);
@@ -107,68 +76,27 @@ namespace TestGenerator
                 MessageBox.Show("Пожалуйста, введите корректные значения для длины и диапазона массива.");
             }
         }
-        private void GenerateArraySortDescAlmost_Click(object sender, RoutedEventArgs e)
+
+        private void SwapTwoRandomElements(int[] array)
         {
-            if (int.TryParse(sizeTextBox.Text, out int size) &&
-                int.TryParse(minValueTextBox.Text, out int minValue) &&
-                int.TryParse(maxValueTextBox.Text, out int maxValue))
+            // Генерация случайных индексов для элементов, которые будут меняться местами
+            int index1 = random.Next(0, array.Length);
+            int index2 = random.Next(0, array.Length);
+
+            // Поиск значений элементов, которые будут меняться местами
+            int value1 = array[index1];
+            int value2 = array[index2];
+
+            // Проверка, чтобы значения элементов отличались
+            while (value1 == value2)
             {
-                int[] numbers = GenerateRandomArray(size, minValue, maxValue);
-                Array.Sort(numbers, (a, b) => b.CompareTo(a));
-
-                // Поиск первого элемента, отличного от первого элемента в массиве
-                int indexToSwap = Array.FindIndex(numbers, x => x != numbers[0]);
-
-                // Поменять первый элемент с найденным элементом
-                if (indexToSwap != -1)
-                {
-                    int temp = numbers[0];
-                    numbers[0] = numbers[indexToSwap];
-                    numbers[indexToSwap] = temp;
-                }
-
-                resultOnedim.Text = string.Join(" ", numbers);
-            }
-            else
-            {
-                MessageBox.Show("Пожалуйста, введите корректные значения для длины и диапазона массива.");
-            }
-        }
-        /*private void GenerateArraySortDescAlmost_Click(object sender, RoutedEventArgs e)
-        {
-            if (int.TryParse(sizeTextBox.Text, out int size) &&
-                int.TryParse(minValueTextBox.Text, out int minValue) &&
-                int.TryParse(maxValueTextBox.Text, out int maxValue))
-            {
-                int[] numbers = GenerateRandomArray(size, minValue, maxValue);
-                Array.Sort(numbers, (a, b) => b.CompareTo(a));
-
-                // "Рушение" порядка путем перемещения одного элемента в начало массива
-                if (numbers.Length > 1)
-                {
-                    int temp = numbers[0];
-                    numbers[0] = numbers[1];
-                    numbers[1] = temp;
-                }
-
-                resultOnedim.Text = string.Join(" ", numbers);
-            }
-            else
-            {
-                MessageBox.Show("Пожалуйста, введите корректные значения для длины и диапазона массива.");
-            }
-        }*/
-        private int[] GenerateRandomArray(int size, int minValue, int maxValue)
-        {
-            Random random = new Random();
-            int[] numbers = new int[size];
-
-            for (int i = 0; i < size; i++)
-            {
-                numbers[i] = random.Next(minValue, maxValue + 1);
+                index2 = random.Next(0, array.Length);
+                value2 = array[index2];
             }
 
-            return numbers;
+            // Меняем местами значения элементов
+            array[index1] = value2;
+            array[index2] = value1;
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
@@ -207,6 +135,89 @@ namespace TestGenerator
                     MessageBox.Show("Ошибка при сохранении файла: " + ex.Message);
                 }
             }
+        }
+
+        private void CreateZip_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(fileCountTextBox.Text, out int fileCount))
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "ZIP Archive (*.zip)|*.zip";
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    string zipFilePath = saveFileDialog.FileName;
+                    int size = int.Parse(sizeTextBox.Text);
+                    int minValue = int.Parse(minValueTextBox.Text);
+                    int maxValue = int.Parse(maxValueTextBox.Text);
+
+                    // Create a new zip archive
+                    using (ZipArchive zipArchive = ZipFile.Open(zipFilePath, ZipArchiveMode.Create))
+                    {
+                        Random random = new Random();
+
+                        // Create the specified number of text files
+                        for (int i = 0; i < fileCount; i++)
+                        {
+                            string fileName = $"Test{i + 1}.txt";
+
+                            // Generate a new random array for each file
+                            int[] numbers = GenerateRandomArray(size, minValue, maxValue);
+
+                            if (standardRadioButton.IsChecked == true)
+                            {
+                                // Do nothing, numbers array is already generated randomly
+                            }
+                            else if (sortAscRadioButton.IsChecked == true)
+                            {
+                                Array.Sort(numbers);
+                            }
+                            else if (sortDescRadioButton.IsChecked == true)
+                            {
+                                Array.Sort(numbers, (a, b) => b.CompareTo(a));
+                            }
+                            else if (sortAscAlmostRadioButton.IsChecked == true)
+                            {
+                                Array.Sort(numbers);
+                                SwapTwoRandomElements(numbers);
+                            }
+                            else if (sortDescAlmostRadioButton.IsChecked == true)
+                            {
+                                Array.Sort(numbers, (a, b) => b.CompareTo(a));
+                                SwapTwoRandomElements(numbers);
+                            }
+
+                            // Create a new entry in the zip archive with the file name
+                            ZipArchiveEntry entry = zipArchive.CreateEntry(fileName);
+
+                            // Write the file content to the entry
+                            using (StreamWriter writer = new StreamWriter(entry.Open()))
+                            {
+                                foreach (int number in numbers)
+                                {
+                                    writer.Write(number + " ");
+                                }
+                            }
+                        }
+                    }
+
+                    //MessageBox.Show("Файлы успешно созданы внутри архива.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректное количество файлов.");
+            }
+        }
+        private int[] GenerateRandomArray(int size, int minValue, int maxValue)
+        {
+            int[] numbers = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = random.Next(minValue, maxValue + 1);
+            }
+
+            return numbers;
         }
     }
 }
